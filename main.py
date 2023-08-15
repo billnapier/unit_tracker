b9e5498b04b8bf89d94d5fbe1624e059ddfcb5d0
Bill Napier <napier@google.com>
2023-08-15 05:21:26 +0000

    Actually render the template.

--- main.py
+++ main.py
@@ -137,8 +137,7 @@ def send_to_units(code, unit_type, subject, units):
                 'from_display_name'), addr_spec=smtp_config.get('from_email'))
             msg['To'] = [c.get('email') for c in get_contacts_from_unit(unit)]
             msg.set_content(code)
-            markdown = markdown2.markdown(code)
-            msg.add_alternative(markdown, subtype='html')
+            msg.add_alternative(markdown2.markdown(render_template_string(code, unit=unit)), subtype='html')
             smtp.send_message(msg)
 
 
